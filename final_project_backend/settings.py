22006a8658179298fae5d2e32e30b2173c7e6c7f
Connall Gallagher <shapeboss@live.ie>
2024-06-26 11:31:14 +0100

    Added JSON Web Token Auth

--- final_project_backend/settings.py
+++ final_project_backend/settings.py
@@ -26,6 +26,41 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [(
+        'rest_framework.authentication.SessionAuthentication'
+        if 'DEV' in os.environ
+        else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+    )],
+    # 'DEFAULT_PAGINATION_CLASS':
+    #     'rest_framework.pagination.PageNumberPagination',
+    # 'PAGE_SIZE': 10,
+    # 'DATETIME_FORMAT': '%d %b %Y',
+}
+
+# if 'DEV' not in os.environ:
+#     REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
+#         'rest_framework.renderers.JSONRenderer',
+#     ]
+
+
+REST_USE_JWT = True
+JWT_AUTH_SECURE = True
+JWT_AUTH_COOKIE = 'my-app-auth'
+JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+# JWT_AUTH_SAMESITE = 'None'
+
+
+REST_AUTH_SERIALIZERS = {
+  'USER_DETAILS_SERIALIZER': 'final_project_backend.serializers.CurrentUserSerializer'
+}
+
+# Wrong code below??
+# REST_AUTH_SERIALIZERS = {
+#   'USER_DETAILS_SERIALIZER': 'drf_api.serializers.CurrentUserSerializer'
+# }
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -50,10 +85,21 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'rest_framework',
+    'rest_framework.authtoken',
+    'dj_rest_auth',
     'django_filters',
+
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'dj_rest_auth.registration',
+
     'profiles',
     'tasks',
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,13 +1,24 @@
 from rest_framework import serializers
 from .models import Profile
+from tasks.models import Task  # Import Task model from the 'tasks' app
+
 
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    completed_tasks_count = serializers.SerializerMethodField()
+    total_tasks_count = serializers.SerializerMethodField()
     #is_owner = serializers.SerializerMethodField()
 
     class Meta:
         model = Profile
         fields = [
-            'id', 'owner', 'created_at', 'updated_at', 'first_name', 'last_name', 'image'
+            'id', 'owner', 'created_at', 'updated_at', 'first_name', 'last_name', 'image', 'completed_tasks_count', 'total_tasks_count',
         ]
+
+
+    def get_completed_tasks_count(self, obj):
+        return obj.owner.tasks.filter(completed=True).count()
+
+    def get_total_tasks_count(self, obj):
+        return obj.owner.tasks.count()
