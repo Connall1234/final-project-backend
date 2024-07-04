5a8f1f3fb2db63970353525e64e103b79cd2e125
Connall Gallagher <shapeboss@live.ie>
2024-07-04 14:10:18 +0100

    Changed deafult permission classes

--- final_project_backend/settings.py
+++ final_project_backend/settings.py
@@ -36,7 +36,7 @@ REST_FRAMEWORK = {
     )],
 
    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAdminUser'
+        'rest_framework.permissions.IsAuthenticated'
     ),
 
     'DEFAULT_PAGINATION_CLASS':
