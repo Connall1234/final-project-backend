848fccc0a58fbf23cd8fb0800827515f8b41b649
Connall Gallagher <shapeboss@live.ie>
2024-08-29 11:25:45 +0100

    Changed profile view and auth for frontend

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from profiles import views
+from .views import ProfileView
 
 urlpatterns = [
-    path('profiles/', views.ProfileList.as_view()),
-    path('profiles/<int:pk>', views.ProfileDetail.as_view()),
- 
+    path('profiles/', ProfileView.as_view(), name='profile-list'),
+    path('profiles/<int:pk>/', ProfileView.as_view(), name='profile-detail'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -2,46 +2,51 @@ from django.http import Http404
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from final_project_backend.permissions import IsOwnerOrReadOnly
-
+from rest_framework.permissions import IsAuthenticated
 from .models import Profile
 from .serializers import ProfileSerializer
 
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
 
-
-
-class ProfileList(APIView):
-    permission_classes = [IsOwnerOrReadOnly]
-
-    """
-    This will list all profiles we have
-    """
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True)
-        return Response(serializer.data)
-
-
-class ProfileDetail(APIView):
-    serializer_class = ProfileSerializer
-    permission_classes = [IsOwnerOrReadOnly]
-    def get_object(self, pk): 
+    def get_object(self, pk):
         try:
             profile = Profile.objects.get(pk=pk)
             return profile
         except Profile.DoesNotExist:
             raise Http404
-    
-    def get(self, request, pk):
-        profile = self.get_object(pk)
-        self.check_object_permissions(self.request, profile)
-        serializer = ProfileSerializer(profile)
+
+    def get(self, request, pk=None):
+        if pk is not None:
+            # Detailed view of a specific profile
+            profile = self.get_object(pk)
+
+            if request.user.is_superuser or profile.owner == request.user:
+                self.check_object_permissions(self.request, profile)
+                serializer = ProfileSerializer(profile)
+                return Response(serializer.data)
+            else:
+                return Response({'detail': 'Not authorized to view this profile.'}, status=status.HTTP_403_FORBIDDEN)
+
+        # Listing view of profiles
+        if request.user.is_superuser:
+            # Superuser: Return all profiles
+            profiles = Profile.objects.all()
+        else:
+            # Regular user: Return only their own profile
+            profiles = Profile.objects.filter(owner=request.user)
+
+        serializer = ProfileSerializer(profiles, many=True)
         return Response(serializer.data)
 
     def put(self, request, pk):
         profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        if request.user.is_superuser or profile.owner == request.user:
+            serializer = ProfileSerializer(profile, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({'detail': 'Not authorized to update this profile.'}, status=status.HTTP_403_FORBIDDEN)
